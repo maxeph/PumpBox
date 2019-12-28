6ab5055828d6c176b44431e176e442a9c316285a
Stephane M <s.manguette@gmail.com>
2019-12-28 11:18:45 +0100

    1. Preparing variables required to get sensor measures and chronos 2. DEBUG mode 3. Comments

--- PumpBox.ino
+++ PumpBox.ino
@@ -1,15 +1,23 @@
-// Set up the global variables linked to the number of level sensors
+const boolean DEBUG = true; // DEBUG mode
+
+// Set up the global variables linked to the number of level sensors TO BE REWORKED
 const byte nblevels = 3;
 byte levels[nblevels];
 byte empt=0,low,med,hi;
-// Set up of global variables linked to the current sensor
+
+// START config Variables (pins, type of sensors, etc)
+// pins
 const int currentPin = A0; // which pin for ACS712
-int mVperAmp = 185; // Which kind of ACS712 use 185 fro 5A Module, 100 for 20A Module, 66 for 30A Module
+int currentPin = nblevels+2
+//config
+int mVperAmp = 185; // Which kind of ACS712 use 185 for 5A Module, 100 for 20A Module, 66 for 30A Module
+const float peakPump = 0.20; // minimum current at which we are sure that the pump is running.
+// END config Variables
 
-
+unsigned long chronoPump, lTime=0;
 
 void setup() {
-  // Set up of levels id might need to be reworked
+  // Set up of levels id MIGHT need to be reworked
   levels[0] = 2;
   levels[1] = 3;
   levels[2] = 4;
@@ -26,13 +34,42 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(getLevel());
+  // Creating temporary variable required for loop
+  unsigned long cTime;
+  float cAmpere;
+  byte cLevel;
+
+  // Getting required measures
+  cTime = millis();
+  cAmpere = getAmpere();
+  cLevel =  getLevel();
+
+  if (cAmpere > peakPump) {
+      chronoPump += (cTime - lTime);
+  }
+
+  // DEBUG
+  if (DEBUG) {
+    Serial.print("timer : ");
+    Serial.println(cTime);
+    Serial.print("Ampère :");
+    Serial.println(cAmpere);
+    Serial.print("cLevel : ");
+    Serial.println(cLevel);
+    Serial.print("L arduino fonctionne depuis ");
+    Serial.println(milli()/1000);
+    Serial.print("La pompe a fonctionné ");
+    Serial.println(chronoPump/1000);
+  }
+
+
+// Preparing next loop
+  lTime=cTime; // time of this loop is last time of next loop
   delay(2000);
 }
 
 byte getLevel() {
-  // Check the levels qnd give back a value
+  // Check the levels and give back a value
   byte level = 0;
   for (int i = 2; i < nblevels+2; i++) {
     level += levels[i-2]*digitalRead(i);
